8a6d10c03321bc979482e455ca65e77b182e7e51
Dang Quoc Huy <huysamdua@yahoo.com>
2018-10-10 21:22:03 +0700

    set debug flag

--- feedme.ino
+++ feedme.ino
@@ -1,12 +1,16 @@
+//#define DEBUG_FLAG;
+
 int led = 13;
 
 void setup() {
   // init the digital pin as an output
   pinMode(led, OUTPUT);
+#ifdef DEBUG_FLAG
   // init serial
   Serial.begin(9600);
   while (!Serial); // wait until Serial is ready
   Serial.println("Welcome, Huy");
+#endif
 }
 
 void blink() {
@@ -19,17 +23,23 @@ void blink() {
 void loop() {
   // get moisture value
   int val = analogRead(A1);
+#ifdef DEBUG_FLAG;
+  Serial.print("moisture value: ");
+  Serial.println(val);
+#endif
   // if below 800, need water
   if (val < 800) {
+#ifdef DEBUG_FLAG
     // output feed me
     Serial.println("I'M HUNGRY! FEED MEEEE!!! >\"<");
+#endif
     // blink the led
     blink();
   } else {
-    // make sure led is low
-    digitalWrite(led, LOW);
+#ifdef DEBUG_FLAG
     // output I'm good
     Serial.println("I'M GOOD!!! ( ^ __ ^ )");
+#endif
     delay(1000);
   }
 }
